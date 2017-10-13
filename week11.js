2b1fedf1eabbaf74821f0c41ad2108d69a556cc6
ibrsal <ibrsaldl@gmail.com>
2017-10-14 00:29:56 +0200

    week1

---
+++ week11.js
@@ -0,0 +1,28 @@
+
+var x= 10.6;
+var y= Math.round(x);
+console.log("rounded value of " + x + "in javascript equal "+ y);
+var colors=["red","green","blue"];
+console.log(colors.length);
+x=10;
+let y1 ="Hello";
+if (x==y1)
+{console.log("SAME TYPE");}
+else
+{console.log("NoT SAME TYPE");}
+x = 7;
+console.log( x % 3);
+var array=[ "abc", 5 , "efg"];
+for ( i=0 ; i< array.length;i++)
+{
+	console.log( array[i]);
+}
+let u = 6/0;
+console.log("u =" + u);
+let u1 = 10/0;
+console.log("u1 =" + u1);
+if (u == u1) 
+{console.log("we can compar undefined");}
+else
+{console.log("we can NOT compar undefined");}
+	
